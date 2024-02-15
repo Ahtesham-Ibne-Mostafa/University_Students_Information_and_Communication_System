62d1dfe3c9c1d6130f725d984496365369464140
Ahtesham Ibne Mostafa <ahtesham.ibne.mostafa@g.bracu.ac.bd>
2024-02-15 20:45:42 +0600

    Update Home.js

--- React/src/Home.js
+++ React/src/Home.js
@@ -1,18 +1,15 @@
-const Home  = () => {
-
+const Home = () => {
     const handleClick = () => {
-        
-    }
+        // Add your desired functionality here
+        console.log("Button clicked!");
+    };
 
-    }
     return (
         <div className="Home">
             <h2>Homepage</h2>
             <button onClick={handleClick}>Click me</button>
         </div>
+    );
+};
 
-
-     );
-}
- 
-export default Home ;
+export default Home;
